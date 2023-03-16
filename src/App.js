e25842c14889f633aa030280257947f1b2b68e74
solahidris <emailsolah@gmail.com>
2023-03-16 17:53:48 +0800

    added onChange on text input label & added a userInputHandler for setUserInput in useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
   // About
@@ -7,6 +8,22 @@ function App() {
   // enter search keys and it filters the user list
   // from the hardcode list based on the entered key
   const list = ["abu", "babu", "cini", "daud", "epul"];
+
+  // One approach is to use a text input with an onChange event handler 
+  // that filters and displays a list of options based on the user's input.
+  // This can be implemented using state
+  // to hold the list of options and the current search query,
+  // and filtering the options based on the query.
+
+  const [userInput, setUserInput] = useState('');
+
+  function userInputHandler(event) {
+    setUserInput(event.target.value);
+  };
+
+  // const userInputHandler(event) => {
+  //   return setUserInput(event.target.value);
+  // };
 
   return (
     <div className="App">
@@ -21,8 +38,9 @@ function App() {
         </p>
         <div className="appContent">
           <label>{list}</label>
-          <input type={"text"} />
-          <textarea />
+          <input type={"text"} value={userInput} onChange={userInputHandler}/>
+          {/* <textarea /> */}
+          <label>this is the current userInput = {userInput}</label>
         </div>
       </header>
     </div>
